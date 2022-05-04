4107fdb139d611cd99e75fa924f541a62bd1ef03
Yana Mironyak <yanika.mironyak@gmail.com>
2022-05-04 16:36:44 +0300

    Update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,39 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+
+galleryRef.addEventListener('click', onClick);
+
+function createListItemsMurkUp() {
+
+    return galleryItems.map(item =>
+        `<div class = "gallery__item"><a href = ${item.original} class = "gallery__link"><img class = "gallery__image" src = ${item.preview} data-source = ${item.original} alt = ${item.description}</a></div>`).join('');
+}
+
+function onClick(evt) {
+    evt.preventDefault();
+    if (evt.target.classList.contains('gallery__image')) {
+        const instance = basicLightbox.create(`<img src = '${evt.target.dataset.source}' width="800" height="600">`);
+    
+        instance.show();      
+
+        window.addEventListener('keydown', onEscKeyPress);
+
+        function onEscKeyPress(evt) {
+            if (evt.code === "Escape") {
+                
+                instance.close();
+            }             
+        }
+    }
+}
+
+const listItems = createListItemsMurkUp();
+
+galleryRef.innerHTML = listItems;
+
+
+ 
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+
 console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+
+function createListItemsMurkUp() {
+
+    return galleryItems.map(item =>
+        `<a  class = "gallery__item" href = ${item.original}><img class = "gallery__image" src = ${item.preview} alt = ${item.description}</a> `).join('');
+}
+
+const listItems = createListItemsMurkUp();
+
+galleryRef.innerHTML = listItems;
+
+new SimpleLightbox('.gallery a', { captionDelay: 250, captionsData: 'alt'});
+ 
+
